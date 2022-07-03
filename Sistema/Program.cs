f9e87ed808459ba8f04a9838170d7f6147f06dc6
Rony Stayne <ronyads33@gmail.com>
2022-07-03 19:29:38 -0300

    Sistemas
    Sistemas

---
+++ Sistema/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Sistema{
+
+    public class Program{
+
+        static void Main(string[] args)
+        {
+            Pessoa PessoaMisteriosa = new Pessoa();
+            PessoaMisteriosa.nome = "Rony Stayne";
+
+            Console.WriteLine(PessoaMisteriosa.nome);
+
+        }
+        
+    }
+}
+
